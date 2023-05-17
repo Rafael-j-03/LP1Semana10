2d2e25868e698b8a8239b9d3e7cbd4b511a08fa4
Rafael José <ra.david@hotmail.com>
2023-05-17 20:10:56 +0100

    Exercise 3
    Add elements to the collections

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -11,6 +11,31 @@ namespace StringCollections
             Stack<string> stack = new Stack<string>();
             Queue<string> queue = new Queue<string>();
             HashSet<string> hashSet = new HashSet<string>();
+
+            // Add elements to the collections
+            list.Add("Apple");
+            list.Add("Banana");
+            list.Add("Durian");
+            list.Add("Cherry");
+            list.Add("Cherry"); // Adding duplicate element
+
+            stack.Push("Apple");
+            stack.Push("Banana");
+            stack.Push("Durian");
+            stack.Push("Cherry");
+            stack.Push("Cherry"); // Adding duplicate element
+
+            queue.Enqueue("Apple");
+            queue.Enqueue("Banana");
+            queue.Enqueue("Durian");
+            queue.Enqueue("Cherry");
+            queue.Enqueue("Cherry"); // Adding duplicate element
+
+            hashSet.Add("Apple");
+            hashSet.Add("Banana");
+            hashSet.Add("Durian");
+            hashSet.Add("Cherry");
+            hashSet.Add("Cherry"); // Adding duplicate element
         }
     }
 }
